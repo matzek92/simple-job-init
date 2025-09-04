d217763b10caf84fb1a2277027f4cd80871a9ca7
Matthias Kasperidus <matthias.kasperidus@remondis.de>
2025-09-04 15:20:11 +0200

    Changes version strings 0.1.0 -> 0.1.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="sji",
-    version="0.1.0",
+    version="0.1.1",
     author="matzek92",
     author_email="matthias@kasperidus.de",
     description="SJI - Eine einfache Python-Bibliothek für Job-Initialisierung",
--- sji/__init__.py
+++ sji/__init__.py
@@ -2,7 +2,7 @@
 SJI - Eine einfache Python-Bibliothek für Job-Initialisierung.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 from .sji import SimpleJobInit
 
